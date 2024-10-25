10976ce6b6ede9f3ac72178bdfc652ecaf7af6fc
AhsanAlphaSquad <ahsan.alphasquad@gmail.com>
2024-10-25 14:53:21 +0500

    LOGS

--- main.go
+++ main.go
@@ -3,18 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
+	"time"
 
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 )
 
 func main() {
+
+	connections := atomic.Int64{}
+
+	ticker := time.NewTicker(5 * time.Second)
+	go func() {
+		for range ticker.C {
+			log.Printf("connections: %d", connections.Load())
+		}
+	}()
+
 	err := http.ListenAndServe(":6969", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		conn, _, _, err := ws.UpgradeHTTP(r, w)
 		if err != nil {
 			log.Printf("upgrade error: %v", err)
 		}
+
+		connections.Add(1)
+
 		go func() {
+			defer connections.Add(-1)
 			defer conn.Close()
 
 			for {
